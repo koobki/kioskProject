92e67b42c4f4de53bac277cd736a130245e1e64f
user <user@PC11-01>
2020-09-14 21:00:41 +0900

    커밋0914

---
+++ src/main/java/edu/sungil/foods/web/domain/dto/MenuInfo.java
@@ -0,0 +1,68 @@
+package edu.sungil.foods.web.domain.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class MenuInfo {
+	
+	private int menuNo;
+	private String menuNm;
+	private int menuPc=0;
+	private String menuDsc;
+	private int menuStock;
+	private String menuDispYn;
+	private String menuImgNm;
+	private MultipartFile fileInfo;
+	public int getMenuNo() {
+		return menuNo;
+	}
+	public void setMenuNo(int menuNo) {
+		this.menuNo = menuNo;
+	}
+	public String getMenuNm() {
+		return menuNm;
+	}
+	public void setMenuNm(String menuNm) {
+		this.menuNm = menuNm;
+	}
+	public int getMenuPc() {
+		return menuPc;
+	}
+	public void setMenuPc(int menuPc) {
+		this.menuPc = menuPc;
+	}
+	public String getMenuDsc() {
+		return menuDsc;
+	}
+	public void setMenuDsc(String menuDsc) {
+		this.menuDsc = menuDsc;
+	}
+	public int getMenuStock() {
+		return menuStock;
+	}
+	public void setMenuStock(int menuStock) {
+		this.menuStock = menuStock;
+	}
+	public String getMenuDispYn() {
+		return menuDispYn;
+	}
+	public void setMenuDispYn(String menuDispYn) {
+		this.menuDispYn = menuDispYn;
+	}
+	public String getMenuImgNm() {
+		return menuImgNm;
+	}
+	public void setMenuImgNm(String menuImgNm) {
+		this.menuImgNm = menuImgNm;
+	}
+	public MultipartFile getFileInfo() {
+		return fileInfo;
+	}
+	public void setFileInfo(MultipartFile fileInfo) {
+		this.fileInfo = fileInfo;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/edu/sungil/foods/web/service/AdminService.java
@@ -0,0 +1,42 @@
+package edu.sungil.foods.web.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import edu.sungil.foods.web.domain.AdminMapper;
+import edu.sungil.foods.web.domain.dto.MenuInfo;
+
+@Service
+@Transactional
+public class AdminService {
+	@Autowired
+	AdminMapper adminMapper;
+	
+	private final Path root = Paths.get("src/main/resources/static/media");
+	
+	
+	public void addMenu(MenuInfo menuInfo) {
+		//1. 파일생성
+		String fileNm = String.valueOf(System.currentTimeMillis())
+				+ "." + StringUtils.getFilenameExtension(menuInfo.getFileInfo().getOriginalFilename());
+		
+		try {
+			Files.copy(menuInfo.getFileInfo().getInputStream(), root.resolve(fileNm));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//2. DB데이터저장
+		menuInfo.setMenuImgNm(fileNm);
+		adminMapper.insertMenuInfo(menuInfo);
+	}
+	
+	
+}
