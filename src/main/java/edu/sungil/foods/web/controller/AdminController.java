e25d4a86d817a19b1c3e7f7f37cdf8315549019f
user <user@DESKTOP-JMVHHN0>
2020-09-16 17:15:05 +0900

    메뉴등록추가

--- src/main/java/edu/sungil/foods/web/controller/AdminController.java
+++ src/main/java/edu/sungil/foods/web/controller/AdminController.java
@@ -1,9 +1,14 @@
 package edu.sungil.foods.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
+
+import edu.sungil.foods.web.domain.dto.MenuInfo;
+import edu.sungil.foods.web.service.AdminService;
 
 /**
  * @author koobk
@@ -11,6 +16,9 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 @RequestMapping(value="/admin")
 public class AdminController {
+	
+	@Autowired
+	AdminService adminService;
 	
 	@RequestMapping(value="",method=RequestMethod.GET)
 	public RedirectView defaultView() {
@@ -23,5 +31,12 @@ public class AdminController {
 	
 	@RequestMapping(value="menuList",method=RequestMethod.GET)
 	public void menuList() {
+	}
+	
+	@RequestMapping(value="menu/add",method=RequestMethod.POST)
+	@ResponseBody
+	public void addMenu(MenuInfo menuInfo) {
+		adminService.addMenu(menuInfo);
+
 	}
 }
---
+++ src/main/java/edu/sungil/foods/web/domain/AdminMapper.java
@@ -0,0 +1,13 @@
+package edu.sungil.foods.web.domain;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import edu.sungil.foods.web.domain.dto.MenuInfo;
+import edu.sungil.foods.web.domain.dto.UserInfo;
+
+@Mapper
+public interface AdminMapper {
+
+	void insertMenuInfo(MenuInfo menuInfo);
+
+}
