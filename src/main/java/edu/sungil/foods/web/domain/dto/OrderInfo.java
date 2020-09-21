90ef7b572fd4eb9a3338e53fb69e98ccc83ab9a3
user <user@PC11-19>
2020-09-21 20:41:41 +0900

    주문

---
+++ src/main/java/edu/sungil/foods/web/domain/dto/OrderInfo.java
@@ -0,0 +1,62 @@
+package edu.sungil.foods.web.domain.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class OrderInfo {
+	private Long menuNo;
+	private Long ordQty;
+	private Long ordNo;
+	private String ordDt;
+	private String menuNm;
+	private Long ordAmt;
+	private String ordStat;
+	
+	public Long getMenuNo() {
+		return menuNo;
+	}
+	public void setMenuNo(Long menuNo) {
+		this.menuNo = menuNo;
+	}
+	public Long getOrderQty() {
+		return ordQty;
+	}
+	public void setOrdQty(Long ordQty) {
+		this.ordQty = ordQty;
+	}
+	public Long getOrdNo() {
+		return ordNo;
+	}
+	public void setOrdNo(Long ordNo) {
+		this.ordNo = ordNo;
+	}
+	public String getOrdDt() {
+		return ordDt;
+	}
+	public void setOrdDt(String ordDt) {
+		this.ordDt = ordDt;
+	}
+	public String getMenuNm() {
+		return menuNm;
+	}
+	public void setMenuNm(String menuNm) {
+		this.menuNm = menuNm;
+	}
+	public Long getOrdAmt() {
+		return ordAmt;
+	}
+	public void setOrdAmt(Long ordAmt) {
+		this.ordAmt = ordAmt;
+	}
+	public String getOrdStat() {
+		return ordStat;
+	}
+	public void setOrdStat(String ordStat) {
+		this.ordStat = ordStat;
+	}
+	public Long getOrdQty() {
+		return ordQty;
+	}
+	
+	
+		
+}
