4b7e59ad729ce66a154d006b47d44e211357cf88
user <user@DESKTOP-JMVHHN0>
2020-09-23 16:12:48 +0900

    주문정보클래스 추가

---
+++ src/main/java/edu/sungil/foods/web/domain/dto/OrderInfo.java
@@ -0,0 +1,59 @@
+package edu.sungil.foods.web.domain.dto;
+
+public class OrderInfo {
+	private Long menuNo;
+	private Long ordQty;
+	private Long ordNo;
+	private String ordDt;
+	private String menuNm;
+	private Long ordAmt;
+	private String ordStat;
+	
+	public Long getMenuNo() {
+		return menuNo;
+	}
+	public void setMenuNo(Long menuNo) {
+		this.menuNo = menuNo;
+	}
+	public Long getOrdQty() {
+		return ordQty;
+	}
+	public void setOrdQty(Long ordQty) {
+		this.ordQty = ordQty;
+	}
+	public Long getOrdNo() {
+		return ordNo;
+	}
+	public void setOrdNo(Long ordNo) {
+		this.ordNo = ordNo;
+	}
+	public String getOrdDt() {
+		return ordDt;
+	}
+	public void setOrdDt(String ordDt) {
+		this.ordDt = ordDt;
+	}
+	public String getMenuNm() {
+		return menuNm;
+	}
+	public void setMenuNm(String menuNm) {
+		this.menuNm = menuNm;
+	}
+	public Long getOrdAmt() {
+		return ordAmt;
+	}
+	public void setOrdAmt(Long ordAmt) {
+		this.ordAmt = ordAmt;
+	}
+	public String getOrdStat() {
+		return ordStat;
+	}
+	public void setOrdStat(String ordStat) {
+		this.ordStat = ordStat;
+	}
+	
+	
+	
+
+
+}
